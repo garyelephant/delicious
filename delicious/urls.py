3cf0925e804f999d389f92f0d54e790d79a6a772
GeiliCode <hi@geilicode.com>
2015-04-17 18:09:44 +0800

    remove examples

--- delicious/urls.py
+++ delicious/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'delicious.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^food/', include('food.urls', namespace="food")),
+    url(r'^admin/', include(admin.site.urls)),
 ]
