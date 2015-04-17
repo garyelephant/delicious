1d9a5c2a9e748fc4a47b3ada034b852e1d400ab2
GeiliCode <hi@geilicode.com>
2015-04-18 07:51:15 +0800

    update urls setting

--- delicious/urls.py
+++ delicious/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^food/', include('food.urls', namespace="food")),
+    url(r'^', include('food.urls', namespace="food")),
     url(r'^admin/', include(admin.site.urls)),
 ]
