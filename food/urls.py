50ad0fdab8e076a861c3afdb2db70f546766ea16
GeiliCode <hi@geilicode.com>
2015-04-18 08:59:00 +0800

    用list view和detail view重构urls和views

--- food/urls.py
+++ food/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^cart/$', views.cart, name='cart'),
-    url(r'^tweet/(?P<tweet_id>[0-9]+)/$', views.tweet_detail, name='tweet_detail'),
+    url(r'^tweet/(?P<pk>[0-9]+)/$', views.TweetDetailView.as_view(), name='tweet_detail'),
     url(r'^tweet/(?P<tweet_id>[0-9]+)/buy/$', views.tweet_buy, name='tweet_buy'),
 ]
