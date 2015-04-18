23550427c97a07a14826490abe03a310cd27c670
GeiliCode <hi@geilicode.com>
2015-04-18 12:34:57 +0800

    add ignore and sae support

--- delicious/wsgi.py
+++ delicious/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import sys
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(BASE_DIR, 'site-packages'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "delicious.settings")
 
